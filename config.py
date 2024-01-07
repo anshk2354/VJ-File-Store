3a027a34f72e7c61c66977ffdeb31d99bc8a322b
anshk2354 <155858616+anshk2354@users.noreply.github.com>
2024-01-07 09:24:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -94,9 +94,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "https://ahdhd.onrender.com"
+        URL = "https://file-to-link-sm0x.onrender.com"
     else:
-        URL = "https://ahdhd.onrender.com"
+        URL = "https://file-to-link-sm0x.onrender.com"
 
 
 
